a7b9e4fbe2d802d2e8738d6b276bb81cf7ac0969
Aaron Keepace <aaron@cubietech.com>
2015-01-06 14:11:19 +0800

    update cubieez rootfs for cbcs

--- cb/cb-cubieez-cs/envsetup.sh
+++ cb/cb-cubieez-cs/envsetup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export CB_PRODUCT_ROOTFS_IMAGE=${CB_PACKAGES_DIR}/cubieez-cb-20140827.tar.gz
+export CB_PRODUCT_ROOTFS_IMAGE=${CB_PACKAGES_DIR}/cubieez-lxde-20140916.tar.gz
 export CB_PRODUCT_ROOTFS_EXT4=${CB_PACKAGES_DIR}/debian-rootfs-20140809.ext4
 export CB_OUTPUT_ROOTFS_EXT4=${CB_OUTPUT_DIR}/debian-rootfs-20140809.ext4
-export CB_ROOTFS_SIZE=1350
+export CB_ROOTFS_SIZE=1700
 export CB_PRODUCT_ONLY_KERNEL=0
 export U_BOOT_WITH_SPL=${CB_PACKAGES_DIR}/u-boot-a10/u-boot-sunxi-with-spl.bin
 export U_BOOT_WITH_SPL_MMC2=${CB_PACKAGES_DIR}/u-boot-a10/u-boot-sunxi-with-spl_mmc2.bin
