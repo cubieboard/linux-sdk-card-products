ed4fd40344a836c2c57603f1f0091a967651cec1
Aaron.Keepace <aaron@cubietech.com>
2015-03-18 10:31:13 +0800

    update debian server rootfs

--- ct/ct-debian-server/envsetup.sh
+++ ct/ct-debian-server/envsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CB_PRODUCT_ROOTFS_IMAGE=${CB_PACKAGES_DIR}/debian-server-rootfs-20140923.tar.gz
+export CB_PRODUCT_ROOTFS_IMAGE=${CB_PACKAGES_DIR}/debian-server-v1.2.tar.gz
 export CB_PRODUCT_ROOTFS_EXT4=${CB_PACKAGES_DIR}/debian-server-20140923.ext4
 export CB_OUTPUT_ROOTFS_EXT4=${CB_OUTPUT_DIR}/debian-server-20140923.ext4
 export CB_ROOTFS_SIZE=755
