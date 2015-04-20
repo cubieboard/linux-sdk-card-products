7b41e4367f0419f94f3893ec1e4842b86ceb76e6
Aaron <aaron@cubietech.com>
2015-04-20 22:05:44 +0800

    mf210 setting for cb2

--- cb2/cb2-linaro-server/envsetup.sh
+++ cb2/cb2-linaro-server/envsetup.sh
@@ -3,7 +3,7 @@
 export CB_PRODUCT_ROOTFS_IMAGE=${CB_PACKAGES_DIR}/linaro-trusty-server-14.04-v1.0.tar.gz
 export CB_PRODUCT_ROOTFS_EXT4=${CB_PACKAGES_DIR}/linaro-trusty-server-20140623-671.ext4
 export CB_OUTPUT_ROOTFS_EXT4=${CB_OUTPUT_DIR}/linaro-trusty-server-20140623-671.ext4
-export CB_ROOTFS_SIZE=775
+export CB_ROOTFS_SIZE=570
 export CB_PRODUCT_ONLY_KERNEL=0
 export U_BOOT_WITH_SPL=${CB_PACKAGES_DIR}/u-boot-a20/u-boot-sunxi-with-spl-cb2-20130111.bin
 export U_BOOT_WITH_SPL_MMC2=${CB_PACKAGES_DIR}/u-boot-a20/u-boot-sunxi-with-spl-mmc2.bin
