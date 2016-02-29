9a51053391c97f214be4f3e2668d344345ed4f20
Sam <sam@cubietech.com>
2016-02-29 16:32:14 +0800

    Update CubieAIO setting

--- cubieaio/cubieaio-linaro-14.04-desktop/envsetup.sh
+++ cubieaio/cubieaio-linaro-14.04-desktop/envsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CB_PRODUCT_ROOTFS_IMAGE=${CB_PACKAGES_DIR}/linaro-desktop-trusty-14.04-no-mesa-egl-Cubieaio-V1.1.tar.gz
-export CB_OUTPUT_ROOTFS_EXT4=${CB_OUTPUT_DIR}/linaro-desktop-trusty-14.04-no-mesa-egl-v1.1.ext4
+export CB_OUTPUT_ROOTFS_EXT4=${CB_OUTPUT_DIR}/linaro-desktop-trusty-14.04-no-mesa-egl-Cubieaio-V1.1.ext4
 export CB_ROOTFS_SIZE=1700
 export CB_BOOTFS_SIZE=61
 export CB_PRODUCT_ONLY_KERNEL=0
